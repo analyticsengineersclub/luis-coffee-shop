6069b6217b18bf6754a64e0caf4b28fcdbe907e4
Ubuntu <ubuntu@ip-172-31-28-168.us-east-2.compute.internal>
2021-08-30 22:16:44 +0000

    new model first time buyer

---
+++ models/monthly_first_orders.sql
@@ -0,0 +1,15 @@
+{{ config(
+    materialized='table',
+    sort='month'
+    ) 
+}}
+
+WITH monthly_first_orders AS (
+    SELECT 
+        DATE_TRUNC(customers.first_order, month) AS month
+        , COUNT( customers.customer_id) AS first_time_buyers
+    FROM {{ ref('customers') }} AS customers
+    GROUP BY 1
+) 
+
+SELECT * FROM monthly_first_orders ORDER BY 1 DESC
