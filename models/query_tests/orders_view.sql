81c2d971df8a08e51dd3834f76cd54f91b149a7f
Ubuntu <ubuntu@ip-172-31-28-168.us-east-2.compute.internal>
2021-09-23 18:06:43 +0000

    comparing view and table materialization

---
+++ models/query_tests/orders_view.sql
@@ -0,0 +1,19 @@
+{{ config(
+    materialized='view'
+    ) 
+}}
+
+with 
+
+orders_monthly_count as (
+
+    select 
+    count(distinct id) as number_of_orders
+    , date_trunc(created_at, month) as created_at_month
+    from {{ source('coffee_shop', 'orders') }}
+    group by 2
+    order by 2 desc
+    
+)
+
+select * from orders_monthly_count
